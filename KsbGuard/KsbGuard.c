c05d3c8503bd7ac616d60e01ebb4e26bfd497eea
peter <peter.niederer@sonnenkinder.org>
2018-06-12 20:18:57 +0200

    first init

---
+++ KsbGuard/KsbGuard.c
@@ -0,0 +1,17 @@
+/*
+ * KsbGuard.c
+ *
+ * Created: 12.06.2018 20:09:44
+ *  Author: mira
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
---
+++ gitAddFiles.bat
@@ -0,0 +1,9 @@
+git add KsbGuard.atsln
+git add KsbGuard.atsuo
+git add KsbGuard/*.h
+git add KsbGuard/*.c
+git add KsbGuard/*.cproj
+git add  KsbGuard/Debug/KsbGuard.hex
+git add gitAddFiles.bat
+
+git status
