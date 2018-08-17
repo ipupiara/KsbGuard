1554e148bf90bd7d3a13c91e5d9a3b2da5d747a6
peter <peter.niederer@sonnenkinder.org>
2018-08-17 08:21:03 +0200

    schematics added

--- gitAddFiles.bat
+++ gitAddFiles.bat
@@ -2,7 +2,9 @@ git add KsbGuard.atsln
 git add KsbGuard.atsuo
 git add KsbGuard/*.h
 git add KsbGuard/*.c
-git add KsbGuard/*.cproj
+git add KsbGuard/*.cprojgit 
+git add KsbSchematics/*.*
+git add KsbSchematics/KsbSchematics/*.*
 git add  KsbGuard/Debug/KsbGuard.hex
 git add gitAddFiles.bat
 
