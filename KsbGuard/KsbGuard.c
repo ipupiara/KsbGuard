eb754333b5dde3acdd6c3189114bbaf109037296
Brigitte Walker <Brigitte Walker>
2018-08-19 10:41:15 +0200

    reg. save during implementation

--- KsbGuard/KsbGuard.c
+++ KsbGuard/KsbGuard.c
@@ -13,8 +13,11 @@
 #include <util/atomic.h>
 
 
-uint16_t  tickCnt;
-uint16_t ticksNeeded;
+uint16_t  tick0Cnt;
+uint16_t ticks0Needed;
+
+uint8_t tick1Cnt;
+uint8_t ledsRunning;
 
 
 int isHandbreakPulled()
@@ -38,49 +41,104 @@ int isPassingBeamOn()
 	return res;
 }
 
-void startLEDs()
+int isEngineRunning()
 {
 	
+}
+
+void startLEDs()
+{
+	ledsRunning = 1;
 }
 
 void stopLEDs()
 {
-	
+	ledsRunning = 0;
 }
 
 void switchLEDs()
 {
+	if (! ledsRunning)  {
+		startLEDs;
+	}
 
 }
 
-void beepShort()
+void startBuzzer()
 {
 	
+}
+
+void stopBuzzer()
+{
+	
+}
+
+void startTimer0()
+{
+	
+}
+
+void stopTimer0()
+{
+	
+}
+
+
+
+void beepTime(uint8_t oc0a)
+{
+	OCR0A = oc0a;
+	
+	startBuzzer();
+	startTimer0();
 }
 
 void beepLong()
 {
-	
+	beepTime(2);
 }
 
 void stopBeep()
 {
-	
+	beepTime(1);
 }
 
 
 ISR(TIMER0_COMPA_vect)
 {
+	cli();
 	
+	
+	stopTimer0();
+	sei();
 }
 
 ISR(TIMER1_COMPA_vect)
 {
+	cli();
+	tick1Cnt += 1;
+	if (tick1Cnt >= 4) { tick1Cnt = 0;}
 	
+	if ( (isKsbPulled()) || ( isEngineRunning() &&  ((! isPassingBeamOn()) || isHandbreakPulled()  )) )	{
+			switchLEDs();
+		}  else {
+			stopLEDs();
+		}
+	}
+	
+	
+	
+	sei();
 }
 
 void setHW()
 {
+	//  set GPIO
+	
+	
+		ledsRunning = 0;
+	
 	// set clock prescaler to 2
 	cli();
 	 //asm volatile (
@@ -91,20 +149,28 @@ void setHW()
 		CLKPR = (1<<CLKPS0);
 		
 	// set timer 0
+		
+		tick0Cnt = 0;
+		ticks0Needed = 20;   // 10 sec
+		TCCR1 = (1 << CTC1) | (1 << CS13) | (1 << CS12)| (1 << CS11)| (1 << CS10) ;   // set prescaler to 16384
+		OCR1A = 244;  // counter top value means approx   1 interrupt per sec
+		GTCCR = 0x00;
+		TIMSK  = 1 << OCIE1A;  //  interrupt needed
+		TCNT1 = 0x00 ;
+		
 	
-	// set timer 1
+	// set Timer 1    
 	
-	// Timer 1    prepare  for ADC triggering
+	tick1Cnt = 0;
 			
-	tickCnt = 0;
-	ticksNeeded = 20;   // 10 sec
 	TCCR1 = (1 << CTC1) | (1 << CS13) | (1 << CS12)| (1 << CS11)| (1 << CS10) ;   // set prescaler to 16384
-	OCR1A = 0xF4;  // counter top value means approx   2 ADC measures per sec
+	OCR1A = 244;  // counter top value means approx   1 interrupt per sec
 	GTCCR = 0x00;
-	TIMSK  = 1 << OCIE1A;  //  interrupt needed for ADC trigger
+	TIMSK  = 1 << OCIE1A;  //  interrupt needed 
 	TCNT1 = 0x00 ;
 	
-		
+	
+	
 		
 	 sei();
 	
